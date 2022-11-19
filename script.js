76e866d7dd317eccde111a6b9415d3635a407d66
Hamda Mouhoumed <75206869+xamdoo@users.noreply.github.com>
2022-11-19 20:04:06 +0300

    solved

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+document.querySelector("#calculate").addEventListener("click", function(){
+    let bill = parseInt (document.querySelector("#bill").value)
+    if(bill > 50 && bill < 300){
+        let tip = bill * 0.15
+        document.querySelector("#result").innerText = `The bill was ${bill}, the tip was ${tip}, and the total value $${bill + tip}`
+    }else if(bill >= 300){
+        let tip = bill * 0.20
+        document.querySelector("#result").innerText = `The bill was ${bill}, the tip was ${tip}, and the total value $${bill+tip}`
+    }
+})
