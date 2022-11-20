4e1b2ca5fc9a3491f56757b669884407f2534b5e
hamda <hamda.baashe44@gmail.com>
2022-11-20 19:55:22 +0000

    finished tip-calculator-dom

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+
+document.getElementById("btn").addEventListener("click", function(){
+    let bill = Number(document.getElementById("input").value)
+      
+       let tip = 0;
+    if(bill >= 50 && bill <= 300){
+        tip = 15/100
+       let total = bill + (bill * tip)
+       document.getElementById("result").innerText = `The bill was ${bill}, the tip was ${tip}, and the total value is ${total}`;
+       
+    }
+      
+    else {
+        tip = 20/100
+         total = bill + (bill * tip)
+         document.getElementById("result").innerText = `The bill was ${bill}, the tip was ${tip}, and the total value is ${total}`
+         
+    }
+    
+})
+
+
