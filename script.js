2c77bd02f7392ba484a4711e7b6fa6ebf77080d1
Abdilahi mohamed <cabdale3377@gmail.com>
2022-11-21 10:29:16 +0300

    making tip calculater using dom

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+
+
+document.querySelector("#calculate").addEventListener("click", function(){
+  let bill = document.querySelector("#billing").value;
+  let tip ;
+  if (bill >= 50 && bill <300){
+    tip = bill * 0.15;
+  }else 
+  tip = bill * 0.20;
+  
+  document.querySelector("#result").innerHTML = `The bill was ${bill} and tip was ${tip} ant the Total value is ${bill + tip}`;
+  
+})
