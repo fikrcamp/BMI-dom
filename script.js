7d2c84742fbb5fbb4996fcb61ddd96dadae80c37
Abdirahman Mohamed (Buushe) <79105703+Buushe@users.noreply.github.com>
2022-11-25 16:06:11 +0600

    done tip dom

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+let calculate = document.querySelector("#calculate").addEventListener("click",function(){
+let bill = document.querySelector("#bil").value
+    
+
+let result = document.querySelector("#result")
+if(bill >= 50 && bill <= 250){
+    result.innerHTML = "tip is 15%";
+}else if(bill > 300){
+    result.innerHTML = "tip is 20%"
+}    
+
+   
+})
