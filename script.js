06249fe070596cc1e93fd47a4b5bb2f7d4187483
unknown <hudeyfiabdirahman6@GMAIL.COM>
2022-11-26 08:42:51 +0300

    Tip caculator dom

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("hello");
+
+
+document.querySelector(#calculate).addeventListener("click",function{
+    document.querySelector("#input").value
+    let tip1=bill*0.15
+    let tip2=bill*0.20
+    let total1=bill+tip1
+    let total2=bill+tip2
+    if (bill >50 && bill >=300) {
+        Document.queryselector("result").innertext=(your bill is $(bill) and your tip is $(tip1) and your total is $(total1) )
+
+    } else(bill>300){
+        document.querySelector("result").innertext=(your bill is $(bill) and your tip is $(tip2 and your total is $(total2)))
+    }
+
+})
